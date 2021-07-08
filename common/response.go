399f573e5c0e7ed8122ef0782982715bbe9a027e
bluefrog <24358102@qq.com>
2021-07-08 22:35:31 +0800

    引入一些公共的结构体

---
+++ common/response.go
@@ -0,0 +1,11 @@
+package common
+
+// 列表统一输出返回结构
+type PageResult struct {
+	List     interface{} `json:"list"`
+	Total    int64       `json:"total"`
+	Page     int         `json:"page"`
+	PageSize int         `json:"pageSize"`
+}
+
+
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module ruoyi-go
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.7.2 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.2
+	gorm.io/gorm v1.21.11
+)
